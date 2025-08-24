9092e0256244524e9f0431e1b9abdaec54ccf2c1
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-08-24 13:59:07 -0400

    initial commit

---
+++ backend_NextFare/src/main/java/com/example/backend_NextFare/BackendNextFareApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_NextFare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNextFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNextFareApplication.class, args);
+	}
+
+}
---
+++ backend_NextFare/src/test/java/com/example/backend_NextFare/BackendNextFareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_NextFare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNextFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
