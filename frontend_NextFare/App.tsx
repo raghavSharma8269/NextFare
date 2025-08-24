bd54bf8d4c5fa42f218280d9da488e31ce82c149
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-08-24 14:32:46 -0400

    added comment for starting command

--- frontend_NextFare/App.tsx
+++ frontend_NextFare/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+// npx expo start
+
 export default function App() {
   return (
     <View style={styles.container}>
