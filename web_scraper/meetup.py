21d0b56df9688e1dc14f752996816d438f123aea
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-08-28 13:26:17 -0400

    removed unused import

--- web_scraper/meetup.py
+++ web_scraper/meetup.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-import re
 import json
 import arrow
 
