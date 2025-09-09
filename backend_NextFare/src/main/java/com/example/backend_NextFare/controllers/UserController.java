25bef8de254e7e9229510c0b21eb3f96a727a42a
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-09-09 12:47:48 -0400

    controller for all user related services

---
+++ backend_NextFare/src/main/java/com/example/backend_NextFare/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.example.backend_NextFare.controllers;
+
+import com.example.backend_NextFare.dto.user.UserDTO;
+import com.example.backend_NextFare.services.user.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(Authentication auth) {
+        return userService.getUser(auth);
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<UserDTO> createOrUpdateProfile(
+            Authentication auth,
+            @RequestBody UserDTO userDTO) {
+        return userService.createOrUpdateUser(userDTO, auth);
+    }
+
+}
