9784f81a50139778a8e7ad3d8529789dde9232d1
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-09-10 16:34:34 -0400

    added method to get all active events

--- backend_NextFare/src/main/java/com/example/backend_NextFare/model/repositories/EventRepository.java
+++ backend_NextFare/src/main/java/com/example/backend_NextFare/model/repositories/EventRepository.java
@@ -2,8 +2,16 @@ package com.example.backend_NextFare.model.repositories;
 
 import com.example.backend_NextFare.model.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT e FROM Event e WHERE e.eventEndTime > :now ORDER BY e.eventEndTime ASC")
+    List<Event> findActiveEvents(@Param("now") LocalDateTime now);
 }
