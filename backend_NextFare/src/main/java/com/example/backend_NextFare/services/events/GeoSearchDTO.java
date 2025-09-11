703a61e6b54d91093069aa5194f8f8994507b074
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-09-10 22:19:32 -0400

    added GeoSearchDTO.java, contains data needed for geosearch query

---
+++ backend_NextFare/src/main/java/com/example/backend_NextFare/services/events/GeoSearchDTO.java
@@ -0,0 +1,14 @@
+package com.example.backend_NextFare.services.events;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class GeoSearchDTO {
+    private double north;
+    private double south;
+    private double east;
+    private double west;
+    private LocalDateTime now;
+}
