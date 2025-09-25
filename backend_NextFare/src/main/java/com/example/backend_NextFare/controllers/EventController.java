cf8f40b8150c8972e4ca2b1f1dfb2d720d007fc7
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-09-25 13:45:55 -0400

    added updated logging

--- backend_NextFare/src/main/java/com/example/backend_NextFare/controllers/EventController.java
+++ backend_NextFare/src/main/java/com/example/backend_NextFare/controllers/EventController.java
@@ -3,6 +3,7 @@ package com.example.backend_NextFare.controllers;
 import com.example.backend_NextFare.model.entities.Event;
 import com.example.backend_NextFare.services.events.EventService;
 import com.example.backend_NextFare.services.events.GeoSearchDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@Slf4j
 public class EventController {
 
     private final EventService eventService;
@@ -21,11 +23,13 @@ public class EventController {
 
     @GetMapping("/active")
     public ResponseEntity<List<Event>> getAllEvents() {
+        log.info("/events/active hit");
         return eventService.getAllActiveEvents();
     }
 
     @GetMapping("/active-in-bounds")
     public ResponseEntity<List<Event>> getActiveEventsInBound(@RequestBody GeoSearchDTO geoSearchDTO) {
+        log.info("/events/active-in-bounds hit");
         return eventService.getActiveEventsInBound(geoSearchDTO);
     }
 
@@ -35,6 +39,7 @@ public class EventController {
             @RequestParam double lng,
             @RequestParam(defaultValue = "2.0") double radiusInMiles)
     {
+        log.info("/events/within-radius hit");
         return eventService.getActiveEventsWithinRadius(lat, lng, radiusInMiles);
     }
 }
