b346cfff95fbd8362d27a18b8d803744cf95c285
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-10-12 12:55:41 -0400

    added types for firebase requests

---
+++ frontend_NextFare/types/auth.ts
@@ -0,0 +1,36 @@
+export interface FirebaseAuthResponse {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+  displayName?: string;
+}
+
+export interface FirebaseRefreshResponse {
+  access_token: string;
+  expires_in: string;
+  token_type: string;
+  refresh_token: string;
+  id_token: string;
+  user_id: string;
+  project_id: string;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  email: string;
+  password: string;
+  username: string;
+}
+
+export interface AuthError {
+  code: string;
+  message: string;
+}
